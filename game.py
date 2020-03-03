590f852bf1b4a2103bfda7c0b2b0e55452049727
itb-ie <42895988+itb-ie@users.noreply.github.com>
2020-03-03 22:41:49 +0100

    Added a homework style version

--- game.py
+++ game.py
@@ -1,23 +1,35 @@
 import random
+# import random is a simple library that implements the random number generator behavior
 
+# Please read into the variables below the correct numbers. Use try and except to catch error.
+# a simple example would be:
+# hero_hp = int(input("how many hp does the hero have?"))
 hero_hp = 50
 dragon_hp = 100
 hero_max_dmg = 20
 dragon_max_dmg = 10
 
-print("The dragon with {} hp attacks out hero with {} hp".format(dragon_hp, hero_hp))
-while True:
+print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
+
+# add a While for an infinite block
+# here goes the while:
     dragon_attack = random.randint(1, dragon_max_dmg)
-    hero_hp = hero_hp - dragon_attack
-    print("The dragon does {} damage and the hero has {} hp left".format(dragon_attack, hero_hp))
-    if hero_hp <= 0:
-        print("Unfortunately the dragon killed  our hero. RIP")
+    # here you need to update the hero hp, you need to subtract the damage that the dragon did
+    # add code on this line
+
+    print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
+    # add an if condition to check if the hero was killed by the dragon
+    # here goes the if
+        print("Unfortunately the dragon killed our hero. RIP sir Bravealot")
         break
 
     hero_attack = random.randint(1, hero_max_dmg)
-    dragon_hp = dragon_hp - hero_attack
-    print("The hero does {} damage and the dragon has {} hp left".format(hero_attack, dragon_hp))
-    if dragon_hp <= 0:
+    # here you need to update the dragon hp, you need to subtract the damage that the hero did
+    # add code on this line
+
+    print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
+    # add an if condition to check if the dragon was killed by the hero
+    # here goes the if
         print("Our valiant hero has slain the dragon!")
         break
 
